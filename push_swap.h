aa5ae2bea4e7945e7de93c8b3863d16cd471a75f
Hasan Kemal ECE <109526257+ecehasankemal@users.noreply.github.com>
2023-01-21 20:48:37 +0300

    Add files via upload

--- push_swap.h
+++ push_swap.h
@@ -13,10 +13,24 @@
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
 
+# include <limits.h>
+# include <stdlib.h>
+# include <unistd.h>
+
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 37
 # endif
 
 char	*get_next_line(int fd);
+
+// all utils
+int		ft_abs(int num);
+int		ft_atoi(const char *str);
+void	ft_putstr(char *str);
+size_t	ft_strlcpy(char *dest, const char *src, size_t size);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+size_t	count_words(const char *s, char c);
+char	*ft_strcreate(const char *s, char c, size_t counter);
+char	**ft_split(char const *s, char c);
 
 #endif
