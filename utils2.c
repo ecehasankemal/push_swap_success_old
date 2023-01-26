871ef863ee370bba05bc344fa256245e81ae9a34
Hasan Kemal ECE <109526257+ecehasankemal@users.noreply.github.com>
2023-01-26 16:35:59 +0300

    Add files via upload

--- utils2.c
+++ utils2.c
@@ -12,47 +12,6 @@
 
 #include "push_swap.h"
 
-size_t	ft_strlcpy(char *dest, const char *src, size_t size)
-{
-	size_t	index;
-
-	index = 0;
-	if (size > 0)
-	{
-		while (index < (size - 1) && src[index] != '\0')
-		{
-			dest[index] = src[index];
-			index++;
-		}
-		dest[index] = '\0';
-	}
-	while (src[index] != '\0')
-	{
-		index++;
-	}
-	return (index);
-}
-
-char	*ft_substr(char const *s, unsigned int start, size_t len)
-{
-	char	*result;
-	size_t	length;
-
-	if (s == NULL)
-		return (NULL);
-	length = ft_strlen((char *)s);
-	if (start > length)
-		return (ft_strdup(""));
-	if (length - start >= len)
-		result = (char *)malloc((len + 1) * sizeof(char));
-	else
-		result = (char *)malloc((length - start + 1) * sizeof(char));
-	if (result == NULL)
-		return (NULL);
-	ft_strlcpy(result, (char *)&s[start], (len + 1));
-	return (result);
-}
-
 size_t	count_words(const char *s, char c)
 {
 	int	index;
@@ -76,46 +35,57 @@ size_t	count_words(const char *s, char c)
 	return (count);
 }
 
-char	*ft_strcreate(const char *s, char c, size_t counter)
+static char	*ft_make_words(char *word, char const *s, int j, int word_ln)
 {
-	size_t	len;
-	size_t	tmp;
+	int	i;
 
-	len = 0;
-	tmp = counter;
-	while (s[tmp] != c && s[tmp] != '\0')
+	i = 0;
+	while (word_ln > 0)
+		word[i++] = s[j - word_ln--];
+	word[i] = 0;
+	return (word);
+}
+
+static char	**ft_split_words(char **res, char const *s, char c, int word_ct)
+{
+	int	i;
+	int	j;
+	int	word_ln;
+
+	i = 0;
+	j = 0 ;
+	word_ln = 0;
+	while (s[j] && i < word_ct)
 	{
-		tmp++;
-		len++;
-	}	
-	return (ft_substr(s, counter, len));
+		while (s[j] && s[j] == c)
+			j++;
+		while (s[j] && s[j] != c)
+		{
+			j++;
+			word_ln++;
+		}
+		res[i] = (char *)malloc(sizeof(char) * (word_ln + 1));
+		if (!res[i])
+			return (0);
+		ft_make_words (res[i], s, j, word_ln);
+		word_ln = 0;
+		i++;
+	}
+	res[i] = 0;
+	return (res);
 }
 
 char	**ft_split(char const *s, char c)
 {
-	size_t	counter;
-	size_t	iter;
+	int		word_ct;
 	char	**res;
 
-	if (!s)
-		return (NULL);
-	res = (char **)malloc((count_words(s, c) + 1) * sizeof(char *));
+	if (s == 0)
+		return (0);
+	word_ct = count_words(s, c);
+	res = (char **)malloc(sizeof(char *) * (word_ct + 1));
 	if (!res)
-		return (NULL);
-	counter = 0;
-	iter = 0;
-	while (s[counter] != '\0')
-	{
-		while (s[counter] == c && s[counter] != '\0')
-			counter++;
-		if (s[counter] != '\0')
-		{
-			res[iter] = ft_strcreate(s, c, counter);
-			iter++;
-		}
-		while (s[counter] != c && s[counter] != '\0')
-			counter++;
-	}
-	res[iter] = 0;
+		return (0);
+	ft_split_words (res, s, c, word_ct);
 	return (res);
 }
